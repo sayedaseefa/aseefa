905bf90b43ea54bab0e6cb489bfc85825ef6d24c
sayedaseefa <81669636+sayedaseefa@users.noreply.github.com>
2021-03-31 14:08:25 +0530

    Add files via upload

---
+++ Big.java
@@ -0,0 +1,19 @@
+class Big
+{
+ public static void main(String a[])
+ {
+ int n1=Integer.parseInt(a[0]);
+ int n2=Integer.parseInt(a[1]);
+ int n3=Integer.parseInt(a[2]);
+ if(n1>n2&&n1>n3)
+ System.out.println("Biggest no="+n1);
+ then(n2>n1&&n2>n3);
+{
+ System.out.println("Biggest no="+n2);
+}
+else if
+{
+System.out.println("Biggest no="+n3);
+}
+}
+}
