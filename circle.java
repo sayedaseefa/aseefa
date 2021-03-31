5d98e6e43b2c30dab8098a1a4dd45e8e95a99303
sayedaseefa <81669636+sayedaseefa@users.noreply.github.com>
2021-03-31 14:06:55 +0530

    Add files via upload

---
+++ circle.java
@@ -0,0 +1,10 @@
+class Circle
+{
+public static void main(String a[])
+  {
+   int radius=10;
+   final float PI=3.14f;
+   float area=PI*radius*radius;
+   system.out.println("Area of Circle="+area);
+  }
+}
